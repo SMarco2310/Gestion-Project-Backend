d72eea85545311b7d921d8d9bb7fa3f640d970ae
SMarco2310 <marc.sossou@ashesi.edu.gh>
2026-08-17 19:21:54 +0000

    feat: add Free tier entitlements fallback config

---
+++ config/entitlements.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'free' => [
+        'max_workspaces' => 2,
+        'max_members' => 5,
+        'max_storage_mb' => 200,
+        'max_attachment_size_mb' => 10,
+        'advanced_analytics' => false,
+        'custom_branding' => false,
+    ],
+];
